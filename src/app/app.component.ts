ec2f4d8b7d0930c22694d0f7a70e8904eded11b4
Sitharthan Vk <sitharthan.v@HCL.COM>
2019-10-31 19:50:45 +0530

    Observe Timer Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
+
+const source = timer(1000, 2000);
+const subscribe = source.subscribe(val => console.log(val));
+setTimeout(() => { subscribe.unsubscribe(); }, 10000);
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sithu-Angular-observable-rxjs';
+  constructor() {
+  }
+}
+
+function sequenceSubscriber(observer) {
+  observer.next('Apple');
+  observer.next('Orange');
+  observer.next('Grappe');
+  observer.complete();
+
+  return {unsubscribe() {}};
 }
