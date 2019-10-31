0d173636ca4e7224c83baffae26b0dd461b5d247
Sitharthan Vk <sitharthan.v@HCL.COM>
2019-10-31 19:54:34 +0530

    Basic Subscribing using Observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { timer } from 'rxjs';
+import { timer, of } from 'rxjs';
 
-const source = timer(1000, 2000);
-const subscribe = source.subscribe(val => console.log(val));
-setTimeout(() => { subscribe.unsubscribe(); }, 10000);
+// const source = timer(1000, 2000);
+// const subscribe = source.subscribe(val => console.log(val));
+// setTimeout(() => { subscribe.unsubscribe(); }, 10000);
 
+const myObservable = of('apple', 'orange', 'grappe');
+const myObserver = {
+  next: (x: string) => console.log('Observer got a next value: ' + x),
+  error: (err: string) => console.log('Observer got an error: ' + err),
+  complete: () => console.log('Observer got a complete notification'),
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,14 +19,7 @@ setTimeout(() => { subscribe.unsubscribe(); }, 10000);
 export class AppComponent {
   title = 'Sithu-Angular-observable-rxjs';
   constructor() {
+    myObservable.subscribe(myObserver);
   }
 }
 
-function sequenceSubscriber(observer) {
-  observer.next('Apple');
-  observer.next('Orange');
-  observer.next('Grappe');
-  observer.complete();
-
-  return {unsubscribe() {}};
-}
