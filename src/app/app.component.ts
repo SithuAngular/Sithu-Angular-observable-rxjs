16f95e503ce9ca0dd9dae76bb3723785a9b0f943
Sitharthan Vk <sitharthan.v@HCL.COM>
2019-10-31 20:00:03 +0530

    The Observable that Publishes Events Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { timer, of, Observable } from 'rxjs';
 
 // const source = timer(1000, 2000);
@@ -11,28 +11,49 @@ import { timer, of, Observable } from 'rxjs';
 //   error: (err: string) => console.log('Observer got an error: ' + err),
 //   complete: () => console.log('Observer got a complete notification'),
 // };
-const sequence = new Observable(sequenceSubscriber);
+// const sequence = new Observable(sequenceSubscriber);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Sithu-Angular-observable-rxjs';
   constructor() {
     // myObservable.subscribe(myObserver);
-    sequence.subscribe({
-      next(msg) { console.log(msg); },
-      complete() { console.log('Finished Sequence'); }
+    // sequence.subscribe({
+    //   next(msg) { console.log(msg); },
+    //   complete() { console.log('Finished Sequence'); }
+    // });
+  }
+  fromEvent(target: HTMLInputElement, eventName: string) {
+    return new Observable((observer) => {
+      const handler = (e: unknown) => observer.next(e);
+      target.addEventListener(eventName, handler);
+      return () => {
+        target.removeEventListener(eventName, handler);
+      };
+    });
+  }
+
+  ngAfterViewInit() {
+    const ESC_KEY = 27;
+    const nameInput = document.getElementById('yourname') as HTMLInputElement;
+    this.fromEvent(nameInput, 'keydown')
+    .subscribe((e: KeyboardEvent) => {
+      // tslint:disable-next-line: deprecation
+      if (e.keyCode === ESC_KEY) {
+        nameInput.value = '';
+      }
     });
   }
 }
 
-function sequenceSubscriber(observer) {
-  observer.next('Apple');
-  observer.next('Orange');
-  observer.next('Grappe');
-  observer.complete();
+// function sequenceSubscriber(observer) {
+//   observer.next('Apple');
+//   observer.next('Orange');
+//   observer.next('Grappe');
+//   observer.complete();
 
-  return {unsubscribe() {}};
-}
+//   return {unsubscribe() {}};
+// }
